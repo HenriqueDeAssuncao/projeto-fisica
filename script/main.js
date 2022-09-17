c3d2e10612acad6592a5e23503f8350812677d87
HenriqueDeAssuncao <henriqueestudante16@gmail.com>
2022-09-17 19:51:07 -0300

    refiz o site

---
+++ script/main.js
@@ -0,0 +1,18 @@
+const btnMenu = document.getElementById('btnMenu');
+const btnHideMenu = document.getElementById('btnHideMenu');
+const menu = document.getElementById('menu');
+btnMenu.addEventListener("click", function showMenu() {
+    menu.style.height = '150px';
+    btnMenu.style.display = 'none';
+    btnHideMenu.style.display = 'block';
+});
+btnHideMenu.addEventListener("click", function hideMenu() {
+    menu.style.height = '0px';
+    btnMenu.style.display = 'block';
+    btnHideMenu.style.display = 'none';
+});
+function fresh() {
+    if (window.innerWidth >= 992) {
+        location.reload();
+    }
+}
