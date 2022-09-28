6be7f9ffa73bc129c0a6d369c7c8ab27f1b3ab74
HenriqueDeAssuncao <henriqueestudante16@gmail.com>
2022-09-28 20:21:38 -0300

    articleIndex

--- script/main.js
+++ script/main.js
@@ -2,7 +2,7 @@ const btnMenu = document.getElementById('btnMenu');
 const btnHideMenu = document.getElementById('btnHideMenu');
 const menu = document.getElementById('menu');
 btnMenu.addEventListener("click", function showMenu() {
-    menu.style.height = '150px';
+    menu.style.height = '159px';
     btnMenu.style.display = 'none';
     btnHideMenu.style.display = 'block';
 });
